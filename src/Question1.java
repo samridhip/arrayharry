4ff1e5cc91d8fb12007dad9b5f12535fd32f2706
samridhip <pandeysamridhi1@gmail.com>
2023-12-20 08:52:29 +0530

    array practice

---
+++ src/Question1.java
@@ -0,0 +1,10 @@
+public class Question1 {
+    public static void main(String[] args) {
+        float[] marks={22f,66f,86f,700f};
+        float sum=0;
+        for(float elements:marks){
+            sum=sum+elements;
+        }
+        System.out.println(sum);
+    }
+}
